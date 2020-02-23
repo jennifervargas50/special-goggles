122fa57bc3b6da22d2f7800b891248d9fb881735
jennifervargas50 <58706171+jennifervargas50@users.noreply.github.com>
2020-02-22 17:20:53 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Name, address and number:
+$firstName = "Jennifer";
+$lastName = "Vargas";
+$street = "1315 W Van Buren St";
+$city = "Phoenix";
+$state = "AZ";
+$zip = 85007;
+$number = "480-550-0152";
+
+
+// Print name, address and number:
+print "<p>My name, address and number are:<br><br>$firstName $lastName<br><br>$street<br>$city $state 
+$zip<br><br>$number</p>";
+
+?>
+</body>
+</html>
