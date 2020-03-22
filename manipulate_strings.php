6c1b72932a51d671bb0f034796642b6c6e90ad83
jennifervargas50 <58706171+jennifervargas50@users.noreply.github.com>
2020-03-21 23:42:55 -0700

    Add files via upload

---
+++ manipulate_strings.php
@@ -0,0 +1,9 @@
+<?php
+
+echo ucwords("jennifer vargas"); // Converts the first character of each word to uppercase
+echo "<br>";
+echo strcmp("Jennifer Vargas","Jennifer"); // String1 is greater than string2
+echo "<br>";
+echo substr("Jennifer Vargas", 9); // Returns Vargas from the string
+
+?>
