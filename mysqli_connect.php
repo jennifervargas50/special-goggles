d7d5105008a9b0ab72bc8df05adde3b1d91e31e9
jennifervargas50 <58706171+jennifervargas50@users.noreply.github.com>
2020-05-04 11:32:07 -0700

    Add files via upload

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,26 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-   <meta charset="utf-8">
-   <title>Connect to MySQL</title>
-</head>
-<body>
-<?php // Script 12.1 - mysqli_connect.php
-/* This script connects to the MySQL database. */
+<?php // Script 13.1 - mysqli_connect.php
+/* This script connects to the database
+and establishes the character set for communications.*/
 
-// Attempt to connect to MYSQL and print out messages:
-if ($dbc = mysqli_connect('localhost', 'root', 'Jv136413', 'myblog')) {
+// Connect:
+$dbc = mysqli_connect('localhost', 'root', 'Jv136413', 'myquotes');
 
-   print '<p>Successfully connected to the database!</p>';
-   
-   mysqli_close($dbc); // Close the connection.
-   
-} else {
-
-   print '<p style="color: red;">Could not connect to the database.</p>';
-   
-}
-
-?>
-</body>
-</html>
+// Set the character set:
+mysqli_set_charset($dbc, 'utf8');
